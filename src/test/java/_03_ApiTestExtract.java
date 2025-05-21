866f986776f691f026b3d1e4de280c301f01c75a
TechnoStudy <ismettechno@gmail.com>
2025-05-21 18:49:28 +0100

    extractingJsonPath3

--- src/test/java/_03_ApiTestExtract.java
+++ src/test/java/_03_ApiTestExtract.java
@@ -24,7 +24,45 @@ public class _03_ApiTestExtract {
         Assert.assertEquals(ulke, "United States");
     }
 
+    @Test
+    public void extractingJsonPath2() {
+        // Soru : "http://api.zippopotam.us/us/90210"  endpoint indne dönen
+        // place dizisinin ilk elemanının state değerinin  "California"
+        // olduğunu testNG Assertion ile doğrulayınız
+
+        String state=
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                //.body("places[0].state", equalTo("California"))
+                .extract().path("places[0].state")  // extract en son komut olmalı
+        ;
+
+        System.out.println("state = " + state); // kendimize kontrol
+        Assert.assertEquals(state,"California"); // assert
+    }
+
+    @Test
+    public void extractingJsonPath3() {
+        // Soru : "https://gorest.co.in/public/v1/users"  endpoint in den dönen
+        // limit bilgisinin 10 olduğunu testNG ile doğrulayınız.
 
 
 
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
