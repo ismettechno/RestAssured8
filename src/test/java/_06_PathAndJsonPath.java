7cdfe551b29066cf0ebe8f6a27a33e9894400b1d
TechnoStudy <ismettechno@gmail.com>
2025-05-22 19:33:55 +0100

    extractingJsonPathIcNesne

--- src/test/java/_06_PathAndJsonPath.java
+++ src/test/java/_06_PathAndJsonPath.java
@@ -1,4 +1,9 @@
+import Model.Place;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 
 public class _06_PathAndJsonPath {
@@ -38,7 +43,7 @@ public class _06_PathAndJsonPath {
                         .then()
                         .log().body()
                         .extract().path("'post code'")   // "post code": "90210",
-                ;
+                ; // String olduğundan hata verecektir
 
         System.out.println("postCode = " + postCode);
     }
@@ -58,5 +63,58 @@ public class _06_PathAndJsonPath {
 
         System.out.println("postCode = " + postCode);
     }
+
+    @Test
+    public void extractingJsonPathIcNesne() {
+
+        List<Place> places=
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .extract().jsonPath().getList("places", Place.class)
+        ;
+
+        System.out.println("places = " + places);
+        // 2. Avantajı : Sadece Place dizisi lazım ise diğerlerini yazmak zorunda değilsin.
+
+        // Daha önceki örneklerde (as) Clas dönüşümleri için tüm yapıya karşılık gelen
+        // gereken tüm classları yazarak dönüştürüp istediğimiz elemanlara ulaşıyorduk.
+
+        // Burada ise(JsonPath) aradaki bir veriyi clasa dönüştürerek bir list olarak almamıza
+        // imkan veren JSONPATH i kullandık.Böylece tek class ile veri alınmış oldu
+        // diğer class lara gerek kalmadan
+
+        // path : class veya tip dönüşümüne imkan veremeyen direk veriyi verir. List<String> gibi
+        // jsonPath : class dönüşümüne ve tip dönüşümüne izin vererek , veriyi istediğimiz formatta verir.
+    }
+
+    @Test
+    public void getUsersV1(){
+        // https://gorest.co.in/public/v1/users  endpointte dönen Sadece Data Kısmını POJO
+        // dönüşümü ile alarak yazdırınız.
+
+
+
+
+    }
+
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
