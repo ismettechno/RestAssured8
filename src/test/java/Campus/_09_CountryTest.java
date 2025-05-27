bc9a6cfd9bb7ff30cf3e017c8a51e8bb15e07b35
TechnoStudy <ismettechno@gmail.com>
2025-05-27 17:41:47 +0100

    Campus CreateCountry

---
+++ src/test/java/Campus/_09_CountryTest.java
@@ -0,0 +1,80 @@
+package Campus;
+
+import com.github.javafaker.Country;
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class _09_CountryTest {
+    Faker randomUreteci = new Faker();
+    RequestSpecification reqSpec;
+    String CountryID = "";
+
+
+    @BeforeClass
+    public void Setup() {
+        // login ol , token al, spec i hazırla
+        Map<String, String> credential = new HashMap<>();
+        credential.put("username", "Campus25");
+        credential.put("password", "Campus.2524");
+        credential.put("rememberMe", "true");
+
+        String token =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(credential)
+
+                        .when()
+                        .post("https://test.mersys.io/auth/login")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().path("access_token");
+
+        System.out.println("token = " + token);
+
+        reqSpec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Bearer " + token)
+                .build();
+    }
+
+    @Test
+    public void CreateCountry() {
+        String ulkeAdi = randomUreteci.address().country() + randomUreteci.number().digits(5);
+        String ulkeKodu = randomUreteci.address().countryCode() + randomUreteci.number().digits(5);
+
+        Map<String, String> newCountry = new HashMap<>();
+        newCountry.put("name", ulkeAdi);
+        newCountry.put("code", ulkeKodu);
+
+        CountryID=
+        given()
+                .spec(reqSpec)
+                .body(newCountry)
+
+                .when()
+                .post("https://test.mersys.io/school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().path("id")
+        ;
+
+        System.out.println("CountryID = " + CountryID);
+    }
+
+
+}
