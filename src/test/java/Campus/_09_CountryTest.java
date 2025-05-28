20c3def4f1e13dd71ca4d8641bc130bb1c0f2486
TechnoStudy <ismettechno@gmail.com>
2025-05-28 19:51:02 +0100

    Campus Jenkins için  hata kaldırıldı

--- src/test/java/Campus/_09_CountryTest.java
+++ src/test/java/Campus/_09_CountryTest.java
@@ -126,7 +126,7 @@ public class _09_CountryTest {
 
                 .then()
                 .log().body()
-                .statusCode(406)  // Jenkins de hata görünmesi için hatalı yapıldı
+                .statusCode(400)
         ;
     }
 
